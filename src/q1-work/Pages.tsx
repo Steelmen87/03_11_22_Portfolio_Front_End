dc47fc741426122c7d6b89fd71541c667e457367
Steel <Steelmen87@bk.ru>
2022-11-03 12:21:51 +0300

    add Test Component ))

---
+++ src/q1-work/Pages.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Navigate, Route, Routes} from "react-router-dom";
+import Error404 from './pages/Error404';
+import Contacts from './pages/Contacts/Contacts';
+import AboutMe from "./pages/AboutMe/AboutMe";
+import Profile from "./pages/Profile";
+import Password_recovery from "./pages/Skills/Skills";
+import Home from "./pages/Header/Home/Home";
+import TestElement from "./pages/TestElement";
+import {BorderTest} from "./pages/Component_for_Test/BorderTest";
+
+export const PATH = {
+    HOME: '/home',
+    ABOUT_ME: '/about-me',
+    PROFILE: '/profile',
+    SKILLS: '/skills',
+    CONTACTS: '/contacts',
+    SEND_MESSAGE: '/send-message',
+    TEST: '/test'
+}
+
+function Pages() {
+    return (
+        <div>
+            <Routes>
+                <Route path={PATH.TEST} element={<TestElement component={<BorderTest/>}/>}/>
+                <Route path={'/'} element={<Navigate to={PATH.HOME}/>}/>
+                <Route path={PATH.HOME} element={<Home/>}/>
+                <Route path={PATH.ABOUT_ME} element={<AboutMe/>}/>
+                <Route path={PATH.PROFILE} element={<Profile/>}/>
+                <Route path={PATH.SKILLS} element={<Password_recovery/>}/>
+                <Route path={PATH.CONTACTS} element={<Contacts/>}/>
+                <Route path={'/*'} element={<Error404/>}/>
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
